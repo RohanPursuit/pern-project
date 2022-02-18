37945a56c5253c2c57d2e06144f9a082b6936376
RohanPursuit <rohansterling@pursuit.org>
2022-02-18 16:09:25 -0500

    update products table with new products

--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,10 +1,10 @@
 \c cta_dev;
 
-INSERT INTO test (name) VALUES
-('Monday'),
-('Tuesday'),
-('Wednesday'),
-('Thursday'),
-('Friday'),
-('Saturday'),
-('Sunday');
+INSERT INTO products (name, description, price, onSale, image) VALUES
+('Monday', 'Sometimes its the first day of the week and sometimes its the second day..??', -1, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
+('Tuesday', 'The day after Monday', 2, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
+('Wednesday', 'Wednesday is just a Monday that is in the middle of the week', 2, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
+('Thursday', 'The day after Wednesday', 40, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
+('Friday', 'What is that bright light, is that..... freedom??', 20000, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
+('Saturday','FREEDOM!', 2, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
+('Sunday', 'The day before Monday', -100, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image');
