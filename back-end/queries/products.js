8b3db2e06e9de8853e64502cecb06ea1675922e6
RohanPursuit <rohansterling@pursuit.org>
2022-02-18 16:10:37 -0500

    add helper functions getProduct, deleteProduct, editProduct

--- back-end/queries/products.js
+++ back-end/queries/products.js
@@ -17,7 +17,7 @@ const getAllProducts = async () => {
 
 const getProduct = async (id) => {
     try{
-        const product = await db.one("SELECT * FROM products WHERE id=$1", id)
+        const product = await db.one("SELECT * FROM products WHERE id=$1", id);
         return {
             success: true,
             payload: product
@@ -30,7 +30,66 @@ const getProduct = async (id) => {
     }
 }
 
+const addProduct = async (item) => {
+    try {
+        const {name, description, price, onSale, image} = item 
+        const product = await db.one(
+        "INSERT INTO products (name, description, price, onSale, image) VALUES($1, $2, $3, $4, $5) RETURNING *",
+        [name, description, price, onSale, image]
+        );
+        return {
+            success: true,
+            payload: product
+        }  
+    } catch (err){
+        return {
+            success: false,
+            payload: err
+        } 
+    }
+}
+
+const deleteProduct = async (id) => {
+    try {
+        const deletedProduct = await db.one(
+        "DELETE FROM products WHERE id=$1 RETURNING *",
+        id
+        );
+        return {
+            success: true,
+            payload: deletedProduct
+        }  
+    } catch (err){
+        return {
+            success: false,
+            payload: err
+        } 
+    }
+};  
+
+const editProduct = async (id, product) => {
+    try {
+        const {name, description, price, onSale, image} = product;
+ 
+      const updatedProduct = await db.one("UPDATE Products SET name=$1, description=$2, price=$3, onSale=$4, image=$5  WHERE id=$6 RETURNING *",
+      [name, description, price, onSale, image, id] );
+      return {
+        success: true,
+        payload: updatedProduct
+        }  
+    } catch (err){
+        return {
+            success: false,
+            payload: err
+        } 
+    }
+    
+}
+
 module.exports = {
     getAllProducts,
     getProduct,
+    addProduct,
+    deleteProduct,
+    editProduct
 }
