a21aec7141e102d565d8b19eb5d11f1dca677941
RohanPursuit <rohansterling@pursuit.org>
2022-02-18 18:07:03 -0500

    add rating and featured to table

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -9,7 +9,9 @@ CREATE TABLE products (
     id SERIAL PRIMARY KEY, 
     name TEXT,
     description TEXT,
-    price INT,
-    sale BOOLEAN,
+    price INT CHECK (price >= 0),
+    sale BOOLEAN DEFAULT false,
+    rating INT CHECK (rating >= 0 AND rating <= 5),
+    featured BOOLEAN DEFAULT false,
     image TEXT
 );
