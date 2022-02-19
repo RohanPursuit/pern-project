2b9bf57e7bf822bf07010cced712cdf39a8d20fd
RohanPursuit <rohansterling@pursuit.org>
2022-02-19 10:21:09 -0500

    updated production schema and seed

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -1,8 +1,14 @@
-\c dcuocq66flc99i
+-- \c dcuocq66flc99i
 
-DROP TABLE IF EXISTS test;
+DROP TABLE IF EXISTS products;
 
-CREATE TABLE test (
+CREATE TABLE products (
     id SERIAL PRIMARY KEY, 
-    name TEXT
+    name TEXT,
+    description TEXT,
+    price INT CHECK (price >= 0),
+    sale BOOLEAN DEFAULT false,
+    rating INT CHECK (rating >= 0 AND rating <= 5),
+    featured BOOLEAN DEFAULT false,
+    image TEXT
 );
