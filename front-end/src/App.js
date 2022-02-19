b0d9c9349750903b87abaeb64ab96726d16ce1c0
RohanPursuit <rohansterling@pursuit.org>
2022-02-18 20:15:17 -0500

    remove test code,  import  Routes and Route from react-router-dom, Home component, Navbar, AllProductPage and ProductDetailPage components

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,20 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
-const API = process.env.REACT_APP_API_URL;
+import {Routes, Route} from "react-router-dom";
+import Home from "./pages/Home";
+import NavBar from "./component/common/NavBar";
+import AllProductsPage from "./pages/AllProductsPage";
+import ProductDetailsPage from "./pages/ProductDetailsPage";
 
-console.log(API);
 function App() {
-  const [days, setDays] = useState([]);
-  useEffect(() => {
-    axios
-      .get(`${API}/test`)
-      .then(
-        (response) => {
-          setDays(response.data);
-        },
-        (error) => console.log("get", error)
-      )
-      .catch((c) => console.warn("catch", c));
-  }, []);
   return (
-    <div>
-      <ul>
-        {days.map((day) => (
-          <li key={day.name}>{day.name}</li>
-        ))}
-      </ul>
+    <div className="App">
+      <NavBar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route path="/products" element={<AllProductsPage/>}/>
+        <Route path="/products/:id" element={<ProductDetailsPage/>}/>
+      </Routes>
     </div>
   );
 }
