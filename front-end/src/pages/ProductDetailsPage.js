bcf8c9d44c698bf9b7544cd06b8ad5374a743c64
RohanPursuit <rohansterling@pursuit.org>
2022-02-18 20:26:05 -0500

    add ProductDetailPage component, import ProductDetailsComponent, export ProductDetailsPage component

---
+++ front-end/src/pages/ProductDetailsPage.js
@@ -0,0 +1,11 @@
+import ProductDetailsComponent from "../component/ProductDetailsComponent";
+
+function ProductDetailsPage(){
+    return (
+        <div className="AllProductsPage">
+            <ProductDetailsComponent/>
+        </div>
+    )
+}
+
+export default ProductDetailsPage;
