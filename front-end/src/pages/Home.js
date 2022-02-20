7121324b3f78452df81080d06f32cfe0e850f3b4
RohanPursuit <rohansterling@pursuit.org>
2022-02-20 11:53:58 -0500

    add map to display all featured content on home page, add all products to home, import AllProductsComponent from ../component/AllProductsComponent, import video from ../videos/Chevrolet.mp4, import useState from react, import Link from react-router-dom, import ./styles/Home.css

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,9 +1,30 @@
-
+import AllProductsComponent from "../component/AllProductsComponent";
+import video from "../videos/Chevrolet.mp4"
+import {useState} from "react"
+import {Link} from "react-router-dom"
+import "../styles/Home.css"
 
 function Home() {
+    const [featured, setFeatured] = useState([])
+
     return (
         <div className="Home">
-            Hello World!
+            <div className="Ad-main">
+                <video src={video} autoPlay muted></video>
+            </div>
+            <div className="Featured-main">
+                {featured.map(product=> {
+                    return (
+                        <div className="Featured-card" key={product.id}>
+                            <Link to={"/products/" + product.id}>
+                                <h4>{product.name}</h4>
+                                <img src={product.image} alt={product.description} />
+                            </Link>
+                        </div>
+                    )
+                })}
+            </div>
+            <AllProductsComponent setFeatured={setFeatured}/>
         </div>
     )
 }
