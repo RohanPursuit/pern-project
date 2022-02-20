88e1ae3ca8bc7b9f020d871373d5c81853d23d3b
RohanPursuit <rohansterling@pursuit.org>
2022-02-20 16:58:27 -0500

    getting all items in cart

--- front-end/src/component/CartComponent.js
+++ front-end/src/component/CartComponent.js
@@ -1,7 +1,28 @@
+import axios from "axios"
+import {useState, useEffect} from "react"
+const URL = process.env.REACT_APP_API_URL
+
 function CartComponent() {
+    const [cart, setCart] = useState([])
+
+    useEffect(() => {
+        axios.get(`${URL}/cart`)
+        .then(response => {
+            setCart(response.data.payload)
+        })
+        .catch(console.log)
+    }, [])
     return(
         <div className="CartComponent">
-            Hello Cart!
+            {cart.map(product => {
+                return(
+                    <div key={product.id} className="Cart-items">
+                        <p>{product.name}</p>
+                        <img src={product.image} alt={product.description} />
+                        <p>{product.price}</p>
+                    </div>
+                )
+            })}
         </div>
     )
 }
