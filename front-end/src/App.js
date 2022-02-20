038859918e28ec5b92c65f051b41048e30404c33
RohanPursuit <rohansterling@pursuit.org>
2022-02-20 11:49:23 -0500

    import App.css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,6 +3,9 @@ import Home from "./pages/Home";
 import NavBar from "./component/common/NavBar";
 import AllProductsPage from "./pages/AllProductsPage";
 import ProductDetailsPage from "./pages/ProductDetailsPage";
+import "./App.css"
+
+
 
 function App() {
   return (
@@ -18,3 +21,24 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+
+
+
+/**
+ * Include a last Item feature where if the item that a customer wants to buy 
+ * is out of stock they can pay any customer that as recently checked out that
+ * item extra money so that the item can be delivered to them instead.
+ * 
+ * When the last item of a product is checked out, all products of that kind
+ * which haven't been delivered yet can be bided on by other users. That money 
+ * goes to the user that checked out the product if they accept the offer.
+ * 
+ * Would like to allow venders to upload images of their product
+ * Noted use google drive
+ * 
+ */
