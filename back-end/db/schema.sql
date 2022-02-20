b57c26ed07dc0c7ad8b141c7d3f5d89092d5537f
RohanPursuit <rohansterling@pursuit.org>
2022-02-20 14:16:23 -0500

    add cart table

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -15,3 +15,14 @@ CREATE TABLE products (
     featured BOOLEAN DEFAULT false,
     image TEXT
 );
+
+CREATE TABLE cart (
+    id SERIAL PRIMARY KEY, 
+    name TEXT,
+    description TEXT,
+    price INT CHECK (price >= 0),
+    sale BOOLEAN DEFAULT false,
+    rating INT CHECK (rating >= 0 AND rating <= 5),
+    featured BOOLEAN DEFAULT false,
+    image TEXT
+)
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -8,3 +8,8 @@ INSERT INTO products (name, description, price, sale, rating, featured, image) V
 ('Friday', 'What is that bright light, is that..... freedom??', 20000, false, 3, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
 ('Saturday','FREEDOM!', 10, false, 5, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
 ('Sunday', 'The day before Monday', 10, true, 1, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image');
+
+
+INSERT INTO cart (name, description, price, sale, rating, featured, image) VALUES
+('Monday', 'Sometimes its the first day of the week and sometimes its the second day..??', 0, true, 0, false, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
+('Tuesday', 'The day after Monday', 2, true, 1, true, 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image');
