d9d66ce2db8c2dccc95e30542b6c3733d758c228
RohanPursuit <rohansterling@pursuit.org>
2022-02-23 17:10:20 -0500

    add bootstrap css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -9,6 +9,7 @@ import VendorProductDetailsPage from "./pages/VendorProductDetailsPage";
 import CreateProductPage from "./pages/CreateProductPage";
 import EditProductPage from "./pages/EditProductPage";
 import {useState} from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
 import "./App.css"
 
 
@@ -18,6 +19,7 @@ function App() {
   return (
     <div className="App">
       <NavBar/>
+      <div className="Route-content">
       <Routes>
         <Route exact path="/" element={<Home/>}/>
         <Route path="/products" element={<AllProductsPage/>}/>
@@ -28,6 +30,7 @@ function App() {
         <Route path="/vendor/products/:id" element={<VendorProductDetailsPage/>}/>
         <Route path="/vendor/products/:id/edit" element={<EditProductPage/>}/>
       </Routes>
+      </div>
     </div>
   );
 }
@@ -42,6 +45,24 @@ export default App;
 
 
 /**
+ * todo after lunch
+ * 1. commit all changes
+ * 1.5 Style NavBar
+ * 2. style large product card with full info - homepage, product page, vender
+ *  product page
+ * 3. style single product info
+ * 4. Cart page, style and receipt -- with some checkout conformation-- should
+ * clear cart after checkout
+ * 5. Dummy sign In page
+ * 
+ * 
+ * Complete user stories 4 to 6 ---- done
+ * no 404 page
+ * 
+ * Didn't create recording.
+ * Don't have readme
+ * 
+ * 
  * Include a last Item feature where if the item that a customer wants to buy 
  * is out of stock they can pay any customer that as recently checked out that
  * item extra money so that the item can be delivered to them instead.
