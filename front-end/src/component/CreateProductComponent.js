b96e4ca0a73c3b9cc669b5e376949556b1f92f94
RohanPursuit <rohansterling@pursuit.org>
2022-02-23 17:10:54 -0500

    import bootstrap components

--- front-end/src/component/CreateProductComponent.js
+++ front-end/src/component/CreateProductComponent.js
@@ -1,63 +1,87 @@
 import {useState} from "react"
 import axios from "axios"
+import Button from "react-bootstrap/Button"
+import Form from 'react-bootstrap/Form'
+import InputGroup from "react-bootstrap/InputGroup"
+import { ArrowRight, Image } from 'react-bootstrap-icons';
+import Row from "react-bootstrap/Row"
+import Col from "react-bootstrap/Col"
+import { useNavigate } from "react-router-dom"
 const URL = process.env.REACT_APP_API_URL
 const VID = process.env.REACT_APP_VID
 
 function CreateProductComponent () {
+    const nav = useNavigate()
     const [product, setProduct] = useState({
         sale: false, 
         image: 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image',
         vid: VID
     })
 
-    const [imageTag, setImageTag] = useState(<input name="Upload Image Instead" id="image" type="text" />)
-
-    // const [imagePreview, setImagePreview] = useState("")
-
-    const handleImageTag = (event) => {
-        console.log(imageTag)
-        if(imageTag.props.type === "text"){
-            setImageTag(<input name="Upload URL Instead" id="image" type="file" accept="image/*" />)
-        } else {
-            setImageTag(<input name="Upload Image Instead" id="image" type="text" />)
-        }
-
-    }
-
     const handleChange = (event) => {
-        if(event.target.id === "price"){
-            setProduct({...product, [event.target.id]: Number(event.target.value)})  
+        if(event.target.name === "price"){
+            setProduct({...product, [event.target.name]: Number(event.target.value)})  
         }else {
-           setProduct({...product, [event.target.id]: event.target.value}) 
+           setProduct({...product, [event.target.name]: event.target.value}) 
         }
         
     }
 
     const handleSubmit = (event) => {
-        console.log(product)
         event.preventDefault()
-        console.log(product)
+        if(product.image.length === 0){
+            product.image = null
+        }
        axios.post(`${URL}/products`, product)
        .then((response) => {
         console.log(response)
+        nav(`/vendor/products/${response.data.payload.id}`)
        })
        .catch(console.log)
     }
 
     return (
-        <div className="CreateProductComponent">
-            <form onChange={handleChange} onSubmit={handleSubmit} action="">
-                <label htmlFor="name">Name: </label>
-                <input id="name" type="text" />
-                <label htmlFor="description">Description: </label>
-                <input id="description" type="text" />
-                <label htmlFor="price">Price: </label>
-                <input id="price" type="number" />
-                <label htmlFor="image">Image: </label>
-                {imageTag}
-                <input type="submit" />
-            </form>
-                <button onClick={handleImageTag}>{imageTag.props.name}</button>
+        <div className="CreateProduct">
+            <Form onChange={handleChange} onSubmit={handleSubmit} action="">
+            <fieldset>
+                <Row>
+                    <Col xs={7}>
+            <Form.Group className="mb-3">
+            <Form.Label htmlFor="price">Name</Form.Label>
+                <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1"><ArrowRight/></InputGroup.Text>
+                <Form.Control size="lg" name="name" type="text" required/>
+                </InputGroup>
+            </Form.Group>
+            </Col>
+            <Col>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="price">Price</Form.Label>
+                <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1">$</InputGroup.Text>
+                <Form.Control size="lg" name="price" type="number" required/>
+                </InputGroup>
+            </Form.Group>
+            </Col>
+            </Row>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="description">Description</Form.Label>
+                <Form.Control size="lg" as="textarea" name="description" type="text" required/>
+            </Form.Group>
+            <Form.Group className="mb-3">
+                <Form.Label htmlFor="image">Image</Form.Label>
+                <InputGroup className="mb-3">
+                <InputGroup.Text id="basic-addon1"><Image/></InputGroup.Text>
+                <Form.Control size="lg" name="image" type="text" placeholder="https:"/>
+                <Button id="button-addon1">
+                    Preview
+                </Button>
+                </InputGroup> 
+                {/* <Button onClick={handleImageTag}>{imageTag.props.name}</Button> */}
+            </Form.Group>
+                <Button className="create-submit-bt" type="submit">Submit</Button>
+            </fieldset>
+            </Form>
         </div>
     )
 }
