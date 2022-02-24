80411d01156691d21139f1ffc3a8945f1c40115c
RohanPursuit <rohansterling@pursuit.org>
2022-02-24 11:15:43 -0500

    removed unused code in return

--- front-end/src/component/AllProductsComponent.js
+++ front-end/src/component/AllProductsComponent.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import {useState, useEffect} from "react"
 import {Link} from "react-router-dom"
+import Card from "react-bootstrap/Card"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
+import Ratio from "react-bootstrap/Ratio"
 const URL = process.env.REACT_APP_API_URL
 
 function AllProductsComponent({setFeatured}){
@@ -22,15 +26,29 @@ function AllProductsComponent({setFeatured}){
         <div className="AllProducts">
             {products.map(product => {
                 return (
-                    <div className="Product-card" key={product.id}>
-                        <Link to={"/products/"+ product.id}>
-                            <img src={product.image} alt={product.description} />
-                            <p>Name: {product.name}</p>
-                            <p>Rating: {product.rating}</p>
-                            <p>Description: {product.description}</p>
-                            <p>Price: {product.price}</p>
-                        </Link>
-                    </div>
+                    <Card as={Link} to={"/products/"+ product.id} key={product.id}>
+                    <Row>
+                    <Col>
+                    <Ratio aspectRatio="1x1">
+                        <Card.Img className="AllProducts-img" variant="top" src={product.image} />
+                    </Ratio>
+                    </Col>
+                    <Col>
+                    <Card.Body>
+                      <Card.Title>{product.name}</Card.Title>
+                      <Card.Text>
+                      Price: ${Number(product.price).toFixed(2)}
+                      </Card.Text>
+                      <Card.Text>
+                      Description: {product.description}
+                      </Card.Text>
+                      <Card.Text>
+                      Rating: {product.rating}
+                      </Card.Text>
+                    </Card.Body>
+                    </Col>
+                    </Row>
+                  </Card>
                     
                 )
             })}
