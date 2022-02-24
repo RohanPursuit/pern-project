0140110f421e60d4b4cfb1e87fdd0826d63bba5a
RohanPursuit <rohansterling@pursuit.org>
2022-02-24 11:52:35 -0500

    add photos

--- back-end/db/prod_seed.sql
+++ back-end/db/prod_seed.sql
@@ -1,17 +1,17 @@
 -- \c dcuocq66flc99i
 
 INSERT INTO products (name, description, price, sale, rating, featured, vid, image) VALUES
-('Monday', 'Sometimes its the first day of the week and sometimes its the second day..??', 0, true, 0, false, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Tuesday', 'The day after Monday', 2, true, 1, true, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Wednesday', 'Wednesday is just a Monday that is in the middle of the week', 2, true, 1, false, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Thursday', 'The day after Wednesday', 40, true, 3, false, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Friday', 'What is that bright light, is that..... freedom??', 20000, false, 3, true, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Saturday','FREEDOM!', 10, false, 5, true, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Sunday', 'The day before Monday', 10, true, 1, true, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Thursday', 'The day after Wednesday', 40, true, 3, false, '2', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Friday', 'What is that bright light, is that..... freedom??', 20000, false, 3, true, '2', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Saturday','FREEDOM!', 10, false, 5, true, '2', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
-('Sunday', 'The day before Monday', 10, true, 1, true, '2', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image');
+('Laptop', 'The Best laptop of 2022', 3000, true, 0, false, '1', 'https://asset.msi.com/resize/image/global/product/product_1_20191210104213_5def06052a389.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png'),
+('Tuesday', 'The day after Monday', 2, true, 1, true, '1', 'https://i.pcmag.com/imagery/reviews/01pr6hmgqz7A5wX5hSQWqRs-1.fit_lim.size_625x365.v1632764534.jpg'),
+('Wednesday', 'Wednesday is just a Monday that is in the middle of the week', 2, true, 1, false, '1', 'https://i.pcmag.com/imagery/reviews/07GmIupznWhAzDQ3Z7li99a-1.fit_lim.size_625x365.v1632849626.jpg'),
+('Thursday', 'The day after Wednesday', 40, true, 3, false, '1', 'https://i.insider.com/60117b551d2df20018b71117?width=1136&format=jpeg'),
+('Friday', 'What is that bright light, is that..... freedom??', 20000, false, 3, true, '1', 'https://media.wired.com/photos/611ae3ef9561b7c69f0785ae/master/w_2400,h_1800,c_limit/Gear_Google-Pixel-5a-5G-SOURCE-Google(1).jpg'),
+('Saturday','FREEDOM!', 10, false, 5, true, '1', 'https://cdn.thewirecutter.com/wp-content/media/2021/08/budget-android-phone-2048px-nord-front.jpg'),
+('Sunday', 'The day before Monday', 10, true, 1, true, '1', 'https://www.trustedreviews.com/wp-content/uploads/sites/54/2021/02/best-Android-phones.jpg'),
+('Thursday', 'The day after Wednesday', 40, true, 3, false, '2', 'https://media.wired.com/photos/60fef8076a5b5b55c0e34a0d/master/pass/Gear-TCL-Series-20-Pro-5G.jpg'),
+('Friday', 'What is that bright light, is that..... freedom??', 20000, false, 3, true, '2', 'https://m-cdn.phonearena.com/images/articles/372321-940/galaxy-z-flip-clamshell-telefoon-1024x676-1.jpg'),
+('Saturday','FREEDOM!', 10, false, 5, true, '2', 'https://www.zdnet.com/a/img/resize/1ab813f2f9530886fc57f449a0ceb6e043678535/2022/02/09/151389f8-881f-43bb-aa1f-937ec0d787ca/best-android-phone.jpg?width=1200&fit=bounds&auto=webp'),
+('Sunday', 'The day before Monday', 10, true, 1, true, '2', 'https://static.swappa.com/static/images/categories/retro/category_phones_600x600.png');
 
 
 INSERT INTO cart (name, description, price, sale, rating, featured, image) VALUES
