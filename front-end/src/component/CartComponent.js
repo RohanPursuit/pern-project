83c096412d5f3d003e491da9a271e5e4a91b9817
RohanPursuit <rohansterling@pursuit.org>
2022-02-24 11:17:30 -0500

    removed unused useEffect and useState, unused code in return

--- front-end/src/component/CartComponent.js
+++ front-end/src/component/CartComponent.js
@@ -1,33 +1,52 @@
-// import {useState, useEffect} from "react"
+import {useNavigate} from 'react-router-dom'
+import Card from "react-bootstrap/Card"
+import Col from "react-bootstrap/Col"
+import Row from "react-bootstrap/Row"
+import Ratio from "react-bootstrap/Ratio"
+import Button from "react-bootstrap/Button"
 
-function CartComponent({cart}) {
-    // const [cart, setCart] = useState([])
+function CartComponent({cart, setCart}) {
+    const nav = useNavigate()
 
-    // useEffect(() => {
-    //     axios.get(`${URL}/cart`)
-    //     .then(response => {
-    //         setCart(response.data.payload)
-    //     })
-    //     .catch(console.log)
-    // }, [])
     return(
         <div className="CartComponent">
+            <h1>Total: ${
+                cart.reduce((a,b) => {
+                 const c = Number(a.price) + (Number(b.price) * Number(b.count))
+                 return {price: c}
+                }, {price: 0}).price.toFixed(2)
+            }</h1>
             {cart.map(product => {
                 return(
-                    <div key={product.id} className="Cart-items">
-                        <p>Name: {product.name}</p>
-                        <img src={product.image} alt={product.description} />
-                        <p>Price: {product.price}</p>
-                        <p>Count: {product.count}</p>
-                    </div>
+                    <Card key={product.id}>
+                           <Row>
+                           <Col>
+                           <Ratio aspectRatio="1x1">
+                               <Card.Img onClick={() => nav(`/products/${product.id}`)} className="CartComponent-img" variant="top" src={product.image} />
+                           </Ratio>
+                           </Col>
+                           <Col>
+                           <Card.Body>
+                             <Card.Title>{product.name}</Card.Title>
+                             <Card.Text>
+                             Price: {Number(product.price).toFixed(2)}
+                             </Card.Text>
+                             <Card.Text>
+                             Description: {product.description}
+                             </Card.Text>
+                             <Card.Text>
+                             Rating: {product.rating}
+                             </Card.Text>
+                             <Card.Text>
+                             Count: {product.count}
+                             </Card.Text>
+                           </Card.Body>
+                           <Button onClick={() => setCart(cart.filter(item => item.id !== product.id))} variant='danger'>Delete</Button>
+                           </Col>
+                           </Row>
+                         </Card>
                 )
             })}
-            {
-                cart.reduce((a,b) => {
-                 const c = a.price + (b.price * b.count)
-                 return {price: c}
-                }, {price: 0}).price
-            }
         </div>
     )
 }
