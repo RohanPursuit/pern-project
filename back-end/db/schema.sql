13f815694985686cfebf95872daa9105f85402ac
RohanPursuit <rohansterling@pursuit.org>
2022-02-24 11:11:41 -0500

    add image and default value for rating

--- back-end/db/schema.sql
+++ back-end/db/schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE products (
     description TEXT,
     price INT CHECK (price >= 0),
     sale BOOLEAN DEFAULT false,
-    rating INT CHECK (rating >= 0 AND rating <= 5),
+    rating INT CHECK (rating >= 0 AND rating <= 5) DEFAULT 1,
     featured BOOLEAN DEFAULT false,
     vid TEXT,
     image TEXT DEFAULT 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'
--- back-end/db/seed.sql
+++ back-end/db/seed.sql
@@ -1,7 +1,7 @@
 \c cta_dev;
 
 INSERT INTO products (name, description, price, sale, rating, featured, vid, image) VALUES
-('Monday', 'Sometimes its the first day of the week and sometimes its the second day..??', 0, true, 0, false, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
+('Monday', 'Sometimes its the first day of the week and sometimes its the second day..??', 0, true, 0, false, '1', 'https://asset.msi.com/resize/image/global/product/product_1_20191210104213_5def06052a389.png62405b38c58fe0f07fcef2367d8a9ba1/1024.png'),
 ('Tuesday', 'The day after Monday', 2, true, 1, true, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
 ('Wednesday', 'Wednesday is just a Monday that is in the middle of the week', 2, true, 1, false, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
 ('Thursday', 'The day after Wednesday', 40, true, 3, false, '1', 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'),
