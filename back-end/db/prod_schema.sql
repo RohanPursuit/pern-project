dda7d776d95ac808366b34767071388b6ba643d1
RohanPursuit <rohansterling@pursuit.org>
2022-02-24 11:52:15 -0500

    drop cart if it exists

--- back-end/db/prod_schema.sql
+++ back-end/db/prod_schema.sql
@@ -1,6 +1,7 @@
 -- \c dcuocq66flc99i
 
 DROP TABLE IF EXISTS products;
+DROP TABLE IF EXISTS cart;
 
 CREATE TABLE products (
     id SERIAL PRIMARY KEY, 
@@ -8,7 +9,7 @@ CREATE TABLE products (
     description TEXT,
     price INT CHECK (price >= 0),
     sale BOOLEAN DEFAULT false,
-    rating INT CHECK (rating >= 0 AND rating <= 5),
+    rating INT CHECK (rating >= 0 AND rating <= 5) DEFAULT 1,
     featured BOOLEAN DEFAULT false,
     vid TEXT,
     image TEXT DEFAULT 'https://dummyimage.com/400x400/6e6c6e/e9e9f5.png&text=No+Image'
